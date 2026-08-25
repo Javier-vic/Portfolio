8a972f7aca12553986be60d34eb250381c139656
Javier Victoriano <93997406+Javier-vic@users.noreply.github.com>
2026-08-24 21:26:29 -0400

    feat: add hero section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Hero } from './components/hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Portfolio');
+
 }
