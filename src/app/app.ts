dc879e8a92de20aeffe178cbefbab4567ed35828
Javier Victoriano <93997406+Javier-vic@users.noreply.github.com>
2026-08-29 15:42:13 -0400

    feat: add footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { Header } from './components/header/header';
 import { Experience } from './components/experience/experience';
 import { Projects } from './components/projects/projects';
 import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Hero, Header, Experience, Projects, Contact],
+  imports: [Hero, Header, Experience, Projects, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
